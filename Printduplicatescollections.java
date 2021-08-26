b7de17e090ab32d2507b933fffbc525c2c127f39
HemaSaravana <88588594+HemaSaravana@users.noreply.github.com>
2021-08-26 23:42:09 +0530

    Add files via upload

--- Printduplicatescollections.java
+++ Printduplicatescollections.java
@@ -18,22 +18,23 @@ hashSet.add(arr[i]);
 }
 
 System.out.println("Array after removed duplicates: "+hashSet);
+
+int[] arr2 = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+List<Integer> dataList= new ArrayList<Integer>(); 
+for (int i=0;i<arr2.length;i++) {
+dataList.add(arr2[i]);
+}
+int len=dataList.size();
+System.out.println("Duplicate elements in given array: ");  
+//Searches for duplicate element  
+for(int i = 0; i < len; i++) {  
+  for(int j = i + 1; j < len; j++) {  
+      if(dataList.get(i) == dataList.get(j))  
+          System.out.println(dataList.get(j));  
+  }  
+}  
 		
-		// get the length of the array
-		// declare an int variable named count
 		
-		// iterate from 0 to the array length-1 (outer loop starts here)
-		
-			// assign 0 to count 
-			
-			// iterate from i to the length of the array by adding 1 to it (inner loop starts here)
-			
-					// compare both the loop variables & check they're equal
-				
-							// increase the count if both the arrays are equal
-					
-			// Out of the inner loop, check and print the first array variable if count is more than 0
-			
 	}
 
 }
